8df249b5a4e3615718ac204917fd8992df4927a2
ayamberkakienam <ayamberkakienam@noreply.github.com>
2017-04-27 21:56:42 +0700

    (Re)initial commit

---
+++ HtmlParser/HtmlParserPrototype/HtmlParserPrototype.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace HtmlParserPrototype
+{
+    class HtmlParserPrototype
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load("https://otomotif.tempo.co/read/news/2017/04/27/295870127/beli-honda-cr-v-turbo-berapa-harganya");
+            if (htmlDoc == null)
+            {
+                Console.WriteLine("htmlDoc NULL");
+            }
+            else
+            {
+                htmlDoc.OptionFixNestedTags = true;
+                HtmlNode artikel = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='artikel']");
+
+                if (artikel == null)
+                {
+                    Console.WriteLine("artikel NULL");
+                }
+                else
+                {
+                    HtmlNodeCollection kontenBerita = artikel.SelectNodes("//p");
+                    foreach (HtmlNode para in kontenBerita) {
+                        Console.WriteLine(para.InnerText);
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PandaMamam.Jar/XmlParserPrototype.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace XmlParser
+{
+    class XmlParserPrototype
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc =  new XmlDocument();
+            doc.Load("http://rss.detik.com/index.php/detikcom");
+            XmlNode node = doc.DocumentElement.SelectSingleNode("/rss/channel");
+
+            foreach (XmlNode item in node)
+            {
+                XmlNode titleNode = item.SelectSingleNode("title");
+                XmlNode urlNode = item.SelectSingleNode("link");
+
+                if (titleNode != null && urlNode != null && item.Name == "item")
+                {
+                    string title = titleNode.InnerText;
+                    string url = urlNode.InnerText;
+                    Console.WriteLine(title);
+                    Console.WriteLine(url);
+                }
+                else
+                {
+                    Console.WriteLine("node: null");
+                }
+
+                Console.WriteLine();
+                // node = node.NextSibling;
+            }
+
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+        }
+    }
+}
